cb1bc7b8517b5a52642a93d1f167f404693b231c
casoa <casoa2019@outlook.com>
2019-07-17 01:05:48 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,58 +1,9 @@
-'use strict';
+"use strict";
 
 module.exports = function (grunt) {
-  var fs = require("fs"),
-    Util = {
-
-      jsBasePath: '_src/',
-      cssBasePath: 'themes/default/_css/',
-
-      fetchScripts: function () {
-
-        var sources = fs.readFileSync("_examples/editor_api.js");
-        sources = /\[([^\]]+\.js'[^\]]+)\]/.exec(sources);
-        sources = sources[1].replace(/\/\/.*[\n\r]/g, '\n').replace(/'|"|\n|\t|\s/g, '');
-        sources = sources.split(",");
-        sources.forEach(function (filepath, index) {
-          sources[index] = Util.jsBasePath + filepath;
-        });
-
-        return sources;
-      },
-
-      fetchStyles: function () {
-
-        var sources = fs.readFileSync(this.cssBasePath + "umeditor.css"),
-          filepath = null,
-          pattern = /@import\s+([^;]+)*;/g,
-          src = [];
-
-        while (filepath = pattern.exec(sources)) {
-          src.push(this.cssBasePath + filepath[1].replace(/'|"/g, ""));
-        }
-
-        return src;
-
-      }
-
-    },
-    pkg = grunt.file.readJSON('package.json'),
-    server = grunt.option('server') || 'php',
-    encode = grunt.option('encode') || 'utf8',
-    disDir = "dist/",
-    zipName = disDir,
-    banner = '/*!\n * UEditor Mini版本\n * version: <%= pkg.version %>\n * build: <%= new Date() %>\n */\n\n';
-
-  //init
-  (function () {
-
-    server = typeof server === "string" ? server.toLowerCase() : 'php';
-    encode = typeof encode === "string" ? encode.toLowerCase() : 'utf8';
-
-    disDir = disDir + encode + '-' + server + '/';
-    zipName = zipName + pkg.name + pkg.version.replace(/\./g, '_') + '-' + encode + '-' + server + '.zip';
-
-  })();
+  const pkg = grunt.file.readJSON('package.json');
+  const disDir = "dist/utf8-php/";
+  const banner = '/*!\n * UEditor Mini版本\n * version: <%= pkg.version %>\n * build: <%= new Date() %>\n */\n\n';
 
   grunt.initConfig({
     pkg: pkg,
@@ -62,11 +13,89 @@ module.exports = function (grunt) {
           banner: banner + '(function($){\n\n',
           footer: '\n\n})(jQuery)'
         },
-        src: Util.fetchScripts(),
+        src: [
+          '_src/editor.js',
+          '_src/core/browser.js',
+          '_src/core/utils.js',
+          '_src/core/EventBase.js',
+          '_src/core/dtd.js',
+          '_src/core/domUtils.js',
+          '_src/core/Range.js',
+          '_src/core/Selection.js',
+          '_src/core/Editor.js',
+          '_src/core/filterword.js',
+          '_src/core/node.js',
+          '_src/core/htmlparser.js',
+          '_src/core/filternode.js',
+          '_src/plugins/inserthtml.js',
+          '_src/plugins/image.js',
+          '_src/plugins/justify.js',
+          '_src/plugins/font.js',
+          '_src/plugins/link.js',
+          '_src/plugins/print.js',
+          '_src/plugins/paragraph.js',
+          '_src/plugins/horizontal.js',
+          '_src/plugins/cleardoc.js',
+          '_src/plugins/undo.js',
+          '_src/plugins/paste.js',
+          '_src/plugins/list.js',
+          '_src/plugins/source.js',
+          '_src/plugins/enterkey.js',
+          '_src/plugins/preview.js',
+          '_src/plugins/basestyle.js',
+          '_src/plugins/video.js',
+          '_src/plugins/selectall.js',
+          '_src/plugins/removeformat.js',
+          '_src/plugins/keystrokes.js',
+          '_src/plugins/autosave.js',
+          '_src/plugins/autoupload.js',
+          '_src/plugins/formula.js',
+          '_src/plugins/xssFilter.js',
+          '_src/ui/widget.js',
+          '_src/ui/button.js',
+          '_src/ui/toolbar.js',
+          '_src/ui/menu.js',
+          '_src/ui/dropmenu.js',
+          '_src/ui/splitbutton.js',
+          '_src/ui/colorsplitbutton.js',
+          '_src/ui/popup.js',
+          '_src/ui/scale.js',
+          '_src/ui/colorpicker.js',
+          '_src/ui/combobox.js',
+          '_src/ui/buttoncombobox.js',
+          '_src/ui/modal.js',
+          '_src/ui/tooltip.js',
+          '_src/ui/tab.js',
+          '_src/ui/separator.js',
+          '_src/ui/scale.js',
+          '_src/adapter/adapter.js',
+          '_src/adapter/button.js',
+          '_src/adapter/fullscreen.js',
+          '_src/adapter/dialog.js',
+          '_src/adapter/popup.js',
+          '_src/adapter/imagescale.js',
+          '_src/adapter/autofloat.js',
+          '_src/adapter/source.js',
+          '_src/adapter/combobox.js'
+        ],
         dest: disDir + '<%= pkg.name %>.js'
       },
       css: {
-        src: Util.fetchStyles(),
+        src: [
+          'themes/default/_css/editor.css',
+          'themes/default/_css/button.css',
+          'themes/default/_css/buttonicon.css',
+          'themes/default/_css/splitbutton.css',
+          'themes/default/_css/combobox.css',
+          'themes/default/_css/comboboxmenu.css',
+          'themes/default/_css/dialog.css',
+          'themes/default/_css/dialogsize.css',
+          'themes/default/_css/popup.css',
+          'themes/default/_css/tab.css',
+          'themes/default/_css/tooltip.css',
+          'themes/default/_css/colorpicker.css',
+          'themes/default/_css/separator.css'
+        ],
         dest: disDir + 'themes/default/css/umeditor.css'
       }
     },
@@ -92,140 +121,49 @@ module.exports = function (grunt) {
       base: {
         files: [
           {
-            src: ['themes/default/images/**', 'dialogs/**', 'third-party/**', 'lang/**'],
+            src: [
+              'themes/default/images/**',
+              'dialogs/**',
+              'third-party/**',
+              'lang/**'
+            ],
             dest: disDir
-
           }
         ]
       },
-      demo: {
-        files: [
-          {
-            src: '_examples/completeDemo.html',
-            dest: disDir + 'index.html'
-          }
-        ]
-      }
     },
-    transcoding: {
-
-      options: {
-        charset: encode
-      },
-      src: [
-        disDir + '*.js',
-        disDir + 'dialogs/*.js',
-        disDir + 'lang/*.js',
-        disDir + '**/*.js',
-        disDir + '**/*.js',
-        disDir + '**/*.js',
-        disDir + '**/*.js',
-        disDir + '**/*.html',
-        disDir + '**/*.css',
-        disDir + '**/*.jsp',
-        disDir + '**/*.java',
-        disDir + '**/*.php',
-        disDir + '**/*.asp',
-        disDir + '**/*.ashx',
-        disDir + '**/*.cs'
-      ]
-
-    },
-    replace: {
-
-      fileEncode: {
-        src: [disDir + '**/*.html', disDir + '**/*.css', disDir + '**/*.php', disDir + '**/*.jsp', disDir + '**/*.net', disDir + '**/*.asp'],
-        overwrite: true,
-        replacements: [{
-          from: /utf-8/gi,
-          to: 'gbk'
-        }]
-      },
-      demo: {
-        src: disDir + 'index.html',
-        overwrite: true,
-        replacements: [{
-          from: /\.\.\//gi,
-          to: ''
-        }, {
-          from: 'editor_api.js',
-          to: '<%= pkg.name %>.min.js'
-        }, {
-          from: '_css',
-          to: 'css'
-        }]
-      },
-      gbkasp: {
-
-        src: [disDir + 'asp/*.asp'],
-        overwrite: true,
-        replacements: [{
-          from: /65001/gi,
-          to: '936'
-        }]
-      }
-
-    },
-    compress: {
-      main: {
-        options: {
-          archive: zipName
-        },
-        expand: true,
-        cwd: disDir,
-        src: ['**/*']
-      }
-    }
-
   });
 
-  grunt.loadNpmTasks('grunt-text-replace');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-closurecompiler');
   grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-transcoding');
-  grunt.loadNpmTasks('grunt-contrib-compress');
 
   grunt.registerTask('default', 'UEditor Mini build', function () {
-
-    var tasks = ['concat', 'cssmin', 'closurecompiler', 'copy:base', 'copy:' + server, 'copy:demo', 'replace:demo'];
-
-    if (encode === 'gbk') {
-      tasks.push('replace:fileEncode');
-      if (server === 'asp') {
-        tasks.push('replace:gbkasp');
-      }
-    }
-
-    tasks.push('transcoding');
-
+    var tasks = [
+      'concat',
+      'cssmin',
+      'closurecompiler',
+      'copy:base'
+    ];
     //config修改
     updateConfigFile();
-
     grunt.task.run(tasks);
-
   });
 
-
   function updateConfigFile () {
-
     var filename = 'umeditor.config.js',
       file = grunt.file.read(filename),
-      path = server + "/",
-      suffix = server === "net" ? ".ashx" : "." + server;
+      path = "php/",
+      suffix = ".php";
 
     file = file.replace(/php\//ig, path).replace(/\.php/ig, suffix);
 
-    //写入到dist
+    // 写入到dist
     if (grunt.file.write(disDir + filename, file)) {
-
       grunt.log.writeln('config file update success');
-
     } else {
       grunt.log.warn('config file update error');
     }
-
   }
-
 };
