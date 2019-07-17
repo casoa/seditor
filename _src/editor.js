db1770c511dd10a90e10973a023345d0b25bcc2b
casoa <casoa2019@outlook.com>
2019-07-17 11:19:28 +0800

    Update editor.js

--- _src/editor.js
+++ _src/editor.js
@@ -1,13 +1,10 @@
 UMEDITOR_CONFIG = window.UMEDITOR_CONFIG || {};
 
 window.UM = {
-    plugins : {},
-
-    commands : {},
-
-    I18N : {},
-
-    version : "1.2.2"
+  plugins: {},
+  commands: {},
+  I18N: {},
+  version: "1.2.2"
 };
 
 var dom = UM.dom = {};
